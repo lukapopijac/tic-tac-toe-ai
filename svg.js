000ee27a39bf7531bcbcf2bb9055e77c7c046385
Luka Popijac <luka.popijac@gmail.com>
2023-06-05 14:36:17 -0700

    svg generate

--- svg.js
+++ svg.js
@@ -1,10 +1,35 @@
-let s = `
+console.log(Deno.args);
+
+let a = Deno.args[0] ?? 'xo.oox.x.';
+
+let board = Number(a.replaceAll('_', ''));
+if(isNaN(board)) {
+	let symbols = a.toLowerCase().split('').map(s => s=='x'?1:s=='o'?2:0);
+	board = 0;
+	for(let symbol of symbols) board = board<<2 | symbol;
+}
+
+let svgBoard = '';
+
+for(let i=0; i<9; i++) {
+	let s = board & 0b11;
+	if(s) {
+		svgBoard += `<use xlink:href="#${s==1?'x':'o'}" x="${4*(1-i%3)}" y="${4*(1-(i/3|0))}"/>\n`
+	}
+	board >>= 2;
+}
+
+// let x = 4*(1-p%3);
+// let y = 4*(1-(p/3|0));
+
+
+let out = `
 <svg 
 	viewBox="0 0 14 14"
 	preserveAspectRatio="xMidYMid"
 	role="img"
-	style="border: 4px solid grey"
-	width="300"
+	width="100"
+	height="100"
 	stroke="black"
 	stroke-width=".2"
 	stroke-linecap="round"
@@ -16,14 +41,9 @@ let s = `
 
 	<g transform="translate(7,7)">
 		<path d="M-2,-6 V6 M2,-6 V6 M-6,-2 H6 M-6,2 H6"/>
-		<use xlink:href="#x" x="-4" y="-4"/>
-		<use xlink:href="#x" x="0"   y="-4"/>
-		<use xlink:href="#x" x="4"  y="0"/>
-		<use xlink:href="#o" x="-4" y="0"/>
-		<use xlink:href="#o" x="0"   y="0"/>
-		<use xlink:href="#x" style="x:0; y:4;"/>
+		${svgBoard}
 	</g>
 </svg>
 `;
 
-document.body.innerHTML = s;
+Deno.writeTextFile('ttt.svg', out);
